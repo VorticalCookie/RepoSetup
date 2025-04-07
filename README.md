928c19335eaabfb5f2ac2b8cf58477de594c0c37
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2025-04-07 12:21:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RepoSetup
+
+Developed with Unreal Engine 5
---
+++ Source/RepoSetup/RepoSetup.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RepoSetup.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RepoSetup, "RepoSetup" );
